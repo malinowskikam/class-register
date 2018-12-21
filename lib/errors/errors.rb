dd6149744284aae93c6d2fa682126794e5bd7eec
malinowskikam <kamiltruskawa@gmail.com>
2018-12-21 20:14:44 +0100

    Model generation errors

---
+++ lib/errors/errors.rb
@@ -0,0 +1,2 @@
+class NoTableError < StandardError
+end
--- lib/models/ModelGenerator.rb
+++ lib/models/ModelGenerator.rb
@@ -1,19 +1,21 @@
+require "lib/errors/errors.rb"
+
 class ModelGeneretor
     self.def generate_models db
-
+        raise NoTableError, 'Brak tabeli "student" w bazie danych' unless db.table_exists? :student
         class Student < Sequel::Model(db[:student])
         end
 
+        raise NoTableError, 'Brak tabeli "note" w bazie danych' unless db.table_exists? :note
         class Note < Sequel::Model(db[:note])
         end
 
+        raise NoTableError, 'Brak tabeli "reprimand" w bazie danych' unless db.table_exists? :reprimand
         class Reprimand < Sequel::Model(db[:reprimand])
         end
 
-        class Reprimand < Sequel::Model(db[:reprimand])
+        raise NoTableError, 'Brak tabeli "subject" w bazie danych' unless db.table_exists? :subject
+        class Reprimand < Sequel::Model(db[:subject])
         end
-
-        class 
-
     end
 end
